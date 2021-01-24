94824fd142d3cab08ceba395ebc6e8e2ee5c49ce
Andrei Dimaano <andreiidimaano@gmail.com>
2021-01-24 00:39:45 -0800

    add menu

---
+++ frontend/bobaful/src/pages/menu.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface menuProps {
+
+}
+
+const Menu: React.FC<menuProps> = ({}) => {
+    return (
+        <div>
+            hello world
+        </div>
+    );
+}
+
+export default Menu
+
