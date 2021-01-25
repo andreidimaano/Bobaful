cd6109e1644086c23f83d6f4b75d1b42f0dbf937
Andrei Dimaano <andreiidimaano@gmail.com>
2021-01-24 18:26:40 -0800

    add boba image

--- frontend/bobaful/src/pages/menu.tsx
+++ frontend/bobaful/src/pages/menu.tsx
@@ -1,3 +1,5 @@
+import { Box } from '@chakra-ui/react';
+import { Image } from "@chakra-ui/image"
 import React from 'react'
 
 interface menuProps {
@@ -5,10 +7,15 @@ interface menuProps {
 }
 
 const Menu: React.FC<menuProps> = ({}) => {
+    const boba = {
+        url: "images/red_boba.png",
+        url2: "https://kitchenconfidante.com/wp-content/uploads/2020/04/Dalgona-Coffee-with-Boba-kitchenconfidante.com-7339-500x375.jpg",
+        alt: "red boba",
+    }
     return (
-        <div>
-            hello world
-        </div>
+        <Box>
+            <Image src={boba.url2} alt={boba.alt} />
+        </Box>
     );
 }
 
