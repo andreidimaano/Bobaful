7e5bb5185c99aa982f1e94f53d794568b317dc51
Andrei Dimaano <andreiidimaano@gmail.com>
2021-02-13 14:06:15 -0800

    add modal

--- frontend/bobaful/src/pages/menu.tsx
+++ frontend/bobaful/src/pages/menu.tsx
@@ -1,4 +1,4 @@
-import { Box, DarkMode, Divider, Flex, Heading, Stat, StatNumber, Text } from '@chakra-ui/react';
+import { Box, Button, DarkMode, Divider, Flex, Heading, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Stat, StatLabel, StatNumber, Text, useDisclosure } from '@chakra-ui/react';
 import Image from 'next/image'
 import React from 'react'
 import { Navbar } from '../components/Navbar';
@@ -9,6 +9,9 @@ interface menuProps {
 }
 
 const Menu: React.FC<menuProps> = ({}) => {
+    type ScrollBehavior = "inside" | "outside";
+    const {isOpen, onOpen, onClose} = useDisclosure();
+    const [scrollBehavior, setScrollBehavior] = React.useState("inside" as ScrollBehavior);
     const boba = {
         url: "images/red_boba.png",
         url2: "https://kitchenconfidante.com/wp-content/uploads/2020/04/Dalgona-Coffee-with-Boba-kitchenconfidante.com-7339-500x375.jpg",
@@ -18,9 +21,9 @@ const Menu: React.FC<menuProps> = ({}) => {
         <>
         <Navbar />
         <Box>
-            <Flex justify="space-between" align="center" p={4}>
-                <Flex direction="column">
-                    <Box width="400px">
+            <Flex as="button" width={"100%"} onClick={onOpen} justify="space-between" align="center" p={4}>
+                <Flex direction="column" textAlign="left">
+                    <Box width="350px">
                         <Heading size="md" >Thai Oat</Heading>
                         <Text>A healthier version of the original thai tea paired with our house oat milk. A definite fan favorite</Text>
                         <Stat>
@@ -28,12 +31,53 @@ const Menu: React.FC<menuProps> = ({}) => {
                         </Stat>
                     </Box>
                 </Flex>
-                <Image src="/images/red_boba.png" alt={boba.alt} width="125" height="125"/>
+                <Box maxW={"83px"} maxH={"125px"}>
+                    <Image src="/images/red_boba_small.png" alt={boba.alt} object-fit={"cover"} width={"auto"} height="125"/>
+                </Box>
             </Flex>
+            <Modal 
+                onClose={onClose} 
+                isOpen={isOpen} 
+                scrollBehavior={scrollBehavior}
+                isCentered
+            >
+                <ModalOverlay align="center" justifyContent="center" />
+                <ModalContent>
+                    <ModalCloseButton />
+                    <ModalHeader> Thai Oat</ModalHeader>
+                    <ModalBody align="center">
+                        <Box maxH={"539px"}>
+                            <Image src="/images/red_boba.png" alt={boba.alt} object-fit={"cover"} width={"auto"} height="300"/>
+                        </Box>
+                        <Text textAlign="left">A healthier version of the original thai tea paired with our house oat milk. A definite fan favorite</Text>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button colorScheme="red" mr={3} onClick={onClose}>
+                            <Text fontWeight={"bold"} >Add to Cart - </Text>
+                            <Text fontWeight={"bold"} >$10.00</Text>
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
             <Divider />
-            <Flex justify="space-between" align="center" p={4}>
-                <Flex direction="column">
-                    <Box width="400px">
+                <Flex as="button" width={"100%"} onClick={onOpen} justify="space-between" align="center" p={4}>
+                    <Flex direction="column" textAlign="left">
+                        <Box width="350px">
+                            <Heading size="md" >Thai Oat</Heading>
+                            <Text>A healthier version of the original thai tea paired with our house oat milk. A definite fan favorite</Text>
+                            <Stat>
+                                <StatNumber>$10.00</StatNumber> 
+                            </Stat>
+                        </Box>
+                    </Flex>
+                    <Box maxW={"83px"} maxH={"125px"}>
+                        <Image src="/images/red_boba_small.png" alt={boba.alt} object-fit={"cover"} width={"auto"} height="125"/>
+                    </Box>
+                </Flex>
+            <Divider />
+            <Flex as="button" width={"100%"} onClick={onOpen} justify="space-between" align="center" p={4}>
+                <Flex direction="column" textAlign="left">
+                    <Box width="350px">
                         <Heading size="md" >Thai Oat</Heading>
                         <Text>A healthier version of the original thai tea paired with our house oat milk. A definite fan favorite</Text>
                         <Stat>
@@ -41,20 +85,9 @@ const Menu: React.FC<menuProps> = ({}) => {
                         </Stat>
                     </Box>
                 </Flex>
-                <Image src="/images/red_boba.png" alt={boba.alt} width="125" height="125"/>
-            </Flex>
-            <Divider />
-            <Flex justify="space-between" align="center" p={4}>
-                <Flex direction="column">
-                    <Box width="400px">
-                        <Heading size="md" >Thai Oat</Heading>
-                        <Text>A healthier version of the original thai tea paired with our house oat milk. A definite fan favorite</Text>
-                        <Stat>
-                            <StatNumber>$10.00</StatNumber> 
-                        </Stat>
-                    </Box>
-                </Flex>
-                <Image src="/images/red_boba.png" alt={boba.alt} width="125" height="125"/>
+                <Box maxW={"83px"} maxH={"125px"}>
+                    <Image src="/images/red_boba_small.png" alt={boba.alt} object-fit={"cover"} width={"auto"} height="125"/>
+                </Box>
             </Flex>
             <Divider />
         </Box>
