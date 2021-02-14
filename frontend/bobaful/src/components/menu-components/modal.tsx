fd80d3f62eb398abc24eb6b1df4799f377885a47
Andrei Dimaano <andreiidimaano@gmail.com>
2021-02-14 14:53:42 -0800

    fix incrementer and decrementer

--- frontend/bobaful/src/components/menu-components/modal.tsx
+++ frontend/bobaful/src/components/menu-components/modal.tsx
@@ -12,24 +12,25 @@ export const MenuItemModal: React.FC<modalProps> = ({priceString}) => {
     let price = parseInt(priceString);
     let width =  (360 / 539 * 300).toString();
 
-    let [state, setQuantity] = useState({quantity: 1, totalPrice: price});
+    let [state, setQuantity] = useState({quantity: 1, totalPrice: (price / 100).toFixed(2)});
     let changePrice = (event) => {
-        setQuantity({quantity: event.target.value, totalPrice: price * event.target.value});
+        if(event.target.value != '-') {
+            setQuantity({quantity: event.target.value, totalPrice: Math.abs(price * event.target.value / 100).toFixed(2)});
+        }
     } 
 
-    let [isDecDisabled, setDecDisabled] = useState(false);
+    let [isDecDisabled, setDecDisabled] = useState(true);
     let [isIncDisabled, setIncDisabled] = useState(false);
     
     let decreasePrice = () => {
         let newQuantity = state.quantity - 1;
         if(isIncDisabled) {
-            console.log("bad");
             setIncDisabled(!isIncDisabled);
         }
-        setQuantity({quantity: newQuantity, totalPrice: price * newQuantity});
-        if(state.quantity - 1 == 1) {
+        if(state.quantity <= 2) {
             setDecDisabled(!isDecDisabled);
         }
+        setQuantity({quantity: newQuantity, totalPrice: (price * newQuantity / 100).toFixed(2)});
     }
 
     let increasePrice = () => {
@@ -37,10 +38,10 @@ export const MenuItemModal: React.FC<modalProps> = ({priceString}) => {
         if(isDecDisabled) {
             setDecDisabled(!isDecDisabled);
         }
-        if(state.quantity + 1 == 6) {
+        if(state.quantity + 1 === 6) {
             setIncDisabled(!isIncDisabled);
         }
-        setQuantity({quantity: newQuantity, totalPrice: price * newQuantity});
+        setQuantity({quantity: newQuantity, totalPrice: (price * newQuantity / 100).toFixed(2)});
     }
 
 
@@ -52,7 +53,7 @@ export const MenuItemModal: React.FC<modalProps> = ({priceString}) => {
                     <Heading size="md" >Thai Oat</Heading>
                     <Text>A healthier version of the original thai tea paired with our house oat milk. A definite fan favorite</Text>
                     <Stat>
-                        <StatNumber>${price}</StatNumber> 
+                        <StatNumber>${(price / 100).toFixed(2)}</StatNumber> 
                     </Stat>
                 </Box>
             </Flex>
@@ -79,7 +80,7 @@ export const MenuItemModal: React.FC<modalProps> = ({priceString}) => {
                 <ModalFooter>
                     <Flex direction="row" justifyContent="flex-end" align="center">
                         <Button isDisabled={isDecDisabled} onClick={decreasePrice} mr={2}>-</Button>
-                        <Input value={state.quantity} onChange={changePrice} textAlign="center" maxW="52px" />
+                        <Input type={"number"} value={state.quantity} onChange={changePrice} textAlign="center" maxW="52px" isReadOnly={true}/>
                         <Button isDisabled={isIncDisabled} onClick={increasePrice}  ml={2}>+</Button>
                         <Button ml={8} colorScheme="red" mr={2} onClick={onClose}>
                             <Text fontWeight={"bold"} >Add to Cart - </Text>
